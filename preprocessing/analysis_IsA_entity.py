139d8998eb8320555123f6c627c2dea3e36ee46f
jiaying.lu <lujiaying93@foxmail.com>
2021-07-11 11:48:52 -0400

    add KG entity percentage analysis

---
+++ preprocessing/analysis_IsA_entity.py
@@ -0,0 +1,87 @@
+"""
+Analysis entity percentage of IsA relation
+Author: Jiaying Lu
+Create Date: Jul 9, 2021
+"""
+import re
+
+import gzip
+import tqdm
+
+
+def analysis_Yago(type_path: str, fact_path: str, file_type: str = 'normal'):
+    entity_set = set()
+    if file_type == 'normal':
+        fopen = open(fact_path)
+    elif file_type == 'gz':
+        fopen = gzip.open(fact_path, 'rt')
+    else:
+        print('invalid file_type')
+        exit(-1)
+    for line in tqdm.tqdm(fopen):
+        line_list = line.strip().split('\t')
+        head = line_list[0]
+        tail = line_list[2]
+        entity_set.add(head)
+        entity_set.add(tail)
+    fopen.close()
+    entity_isA_set = set()
+    if file_type == 'normal':
+        fopen = open(type_path)
+    elif file_type == 'gz':
+        fopen = gzip.open(type_path, 'rt')
+    else:
+        print('invalid file_type')
+        exit(-1)
+    for line in tqdm.tqdm(fopen):
+        line_list = line.strip().split('\t')
+        head = line_list[0]
+        tail = line_list[2]
+        entity_isA_set.add(head)
+        entity_isA_set.add(tail)
+    fopen.close()
+    isA_cnt = len(entity_isA_set)
+    all_cnt = len(entity_set)
+    print('#IsA=%d over %d, %.2f%%' % (isA_cnt, all_cnt, isA_cnt / all_cnt * 100))
+
+
+def analysis_DBpedia(DBpedia_type_path: str, DBpedia_fact_path: str):
+    regex = re.compile(r'[^"\s]\S*|".+?"@en')
+    error_cnt = 0
+    entity_set = set()
+    with open(DBpedia_fact_path) as fopen:
+        for line in tqdm.tqdm(fopen):
+            line_list = regex.findall(line.strip())
+            if len(line_list) != 4:
+                error_cnt += 1
+                continue
+            head = line_list[0]
+            tail = line_list[2]
+            if head.startswith('<http://dbpedia.org'):
+                entity_set.add(head)
+            if tail.startswith('<http://dbpedia.org'):
+                entity_set.add(tail)
+    entity_isA_set = set()
+    with open(DBpedia_type_path) as fopen:
+        for line in tqdm.tqdm(fopen):
+            line_list = line.strip().split(' ')
+            head = line_list[0]
+            entity_isA_set.add(head)
+    print('before union all_cnt=%d' % (len(entity_set)))
+    isA_cnt = len(entity_isA_set)
+    entity_set = entity_set.union(entity_isA_set)
+    all_cnt = len(entity_set)
+    print('#IsA=%d over %d, %.2f%%' % (isA_cnt, all_cnt, isA_cnt / all_cnt * 100))
+
+
+if __name__ == '__main__':
+    Yago_en_type_path = 'data/Yago/en/yago-wd-full-types.nt'
+    Yago_en_fact_path = 'data/Yago/en/yago-wd-facts.nt'
+    # analysis_Yago(Yago_en_type_path, Yago_en_fact_path)
+    Yago_full_type_path = 'data/Yago/full/yago-wd-full-types.nt.gz'
+    Yago_full_fact_path = 'data/Yago/full/yago-wd-facts.nt.gz'
+    # analysis_Yago(Yago_full_type_path, Yago_full_fact_path, 'gz')
+
+    DBpedia_type_path = 'data/DBpedia/instance-types_lang=en_specific.ttl'
+    DBpedia_fact_path = 'data/DBpedia/infobox-properties_lang=en.ttl'
+    # analysis_DBpedia(DBpedia_type_path, DBpedia_fact_path)
--- preprocessing/process_conceptnet.py
+++ preprocessing/process_conceptnet.py
@@ -28,12 +28,35 @@ def extract_edges_in_English(data_path: str, out_path: str):
                 fwrite.write(line)
 
 
+def calculate_entity_associate_with_isA(data_path):
+    entity_set = set()
+    entity_isA_set = set()
+    with open(data_path) as fopen:
+        for line in tqdm.tqdm(fopen):
+            line_list = line.strip().split('\t')
+            rel = line_list[1]
+            head = line_list[2]
+            tail = line_list[3]
+            entity_set.add(head)
+            entity_set.add(tail)
+            if rel == '/r/IsA':
+                entity_isA_set.add(head)
+                entity_isA_set.add(tail)
+    print('#IsA=%d over %d, %.2f%%' % (len(entity_isA_set), len(entity_set),
+                                       len(entity_isA_set) / len(entity_set) * 100))
+
+
 if __name__ == "__main__":
     data_path = 'data/conceptnet/conceptnet-assertions-5.7.0.csv'
-    out_path = 'data/conceptnet/conceptnet-assertions-5.7.0.en.csv'
-    # extract_edges_in_English(data_path, out_path)
+    data_en_path = 'data/conceptnet/conceptnet-assertions-5.7.0.en.csv'
+    # extract_edges_in_English(data_path, data_en_path)
 
     data_path = 'data/conceptnet/conceptnet-assertions-5.7.0.en.csv'
     concept = 'artificial_intelligence'
     out_path = 'data/conceptnet/edges_of_concept/%s.csv' % (concept)
-    extract_edges_contains_concept(data_path, concept, out_path)
+    # extract_edges_contains_concept(data_path, concept, out_path)
+
+    data_path = 'data/conceptnet/conceptnet-assertions-5.7.0.csv'
+    # calculate_entity_associate_with_isA(data_path)
+    data_en_path = 'data/conceptnet/conceptnet-assertions-5.7.0.en.csv'
+    # calculate_entity_associate_with_isA(data_en_path)
